cde802abff490f0925844a310a28aeddd0c09519
RaZer-RovEnz <74563842+RaZer-RovEnz@users.noreply.github.com>
2020-12-18 17:13:01 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+//entry_host
+
+$a = "facebook.net";
+
+$b = "shshbswjhs.lk";
+
+$c = "google.com";
+
+$d = "whatsapp.net";
+
+$e = "dialog.lk";
+
+$f = "sex.com";
+
+$g = "sjss.lk";
+
+$h = "jjjjjs.com";
+
+$i = "ott-catchup.revlet.net";
+
+$j = "fuck.com";
